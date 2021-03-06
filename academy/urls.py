2de7f52f94baf3a311e28b4b609b708f2cf7ae6c
Poli ReeK <polireek@gmail.com>
2021-03-06 13:24:48 +0200

    homework 7

--- academy/urls.py
+++ academy/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('get_students/', views.get_students),
     path('get_lecturers/', views.get_lecturers),
     path('get_groups/', views.get_groups),
+    path('add_student/', views.add_student),
+    path('add_teacher/', views.add_teacher),
+    path('add_course/', views.add_group),
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Group, Lecturer, Student
+from .forms import AddStudentForm, AddLecturerForm, AddGroupForm
 
 
 def get_students(request):
@@ -19,3 +20,36 @@ def get_groups(request):
     groups = Group.objects.all().order_by('-course')
     return render(request, 'groups/get_groups.html',
                   {'groups': groups})
+
+def add_student(request):
+    form = AddStudentForm()
+    if request.method == 'POST':
+        comment_form = AddStudentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_student = comment_form.save(commit=False)
+            new_student.save()
+
+    return render(request, 'students/add_student.html', {'form': form})
+
+
+def add_teacher(request):
+    form = AddLecturerForm()
+    if request.method == 'POST':
+        comment_form = AddLecturerForm(data=request.POST)
+        if comment_form.is_valid():
+            new_teacher = comment_form.save(commit=False)
+            new_teacher.save()
+
+    return render(request, 'teachers/add_teacher.html', {'form': form})
+
+
+def add_group(request):
+    form = AddGroupForm()
+    if request.method == 'POST':
+        comment_form = AddGroupForm(data=request.POST)
+        if comment_form.is_valid():
+            new_course = comment_form.save(commit=False)
+            new_course.save()
+
+    return render(request, 'groups/add_group.html', {'form': form})
+
