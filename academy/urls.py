3292ef4420c9d9c3be3d8efb5f3e9d89ab57c4f9
Poli ReeK <polireek@gmail.com>
2021-03-10 12:22:47 +0200

    homework_8

--- academy/urls.py
+++ academy/urls.py
@@ -3,10 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_students/', views.get_students),
-    path('get_lecturers/', views.get_lecturers),
-    path('get_groups/', views.get_groups),
-    path('add_student/', views.add_student),
-    path('add_teacher/', views.add_teacher),
-    path('add_course/', views.add_group),
+    path('get_students/', views.get_students, name='get_students'),
+    path('get_lecturers/', views.get_lecturers, name='get_lecturers'),
+    path('get_groups/', views.get_groups, name='get_groups'),
+    path('add_student/', views.add_student, name='add_student'),
+    path('add_teacher/', views.add_teacher, name='add_teacher'),
+    path('add_course/', views.add_group, name='add_course'),
+    path('', views.index, name='index'),
+    path('get_student/<int:id>/edit/', views.edit_student, name='edit_student'),
+    path('get_student/<int:id>/delete/', views.delete_student, name='delete_student'),
+    path('get_lecturer/<int:id>/edit/', views.edit_lecturer, name='edit_lecturer'),
+    path('get_lecturer/<int:id>/delete/', views.delete_lecturer, name='delete_lecturer'),
+    path('get_group/<int:id>/edit/', views.edit_group, name='edit_group'),
+    path('get_group/<int:id>/delete/', views.delete_group, name='delete_group')
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,23 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Group, Lecturer, Student
 from .forms import AddStudentForm, AddLecturerForm, AddGroupForm
 
 
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
 def get_students(request):
-    Students = Student.objects.all().order_by('-first_name')
+    students = Student.objects.all().order_by('first_name')
     return render(request, 'students/get_students.html',
-                  {'students': Students})
+                  {'students': students})
 
 
 def get_lecturers(request):
-    lecturers = Lecturer.objects.all().order_by('-first_name')
+    lecturers = Lecturer.objects.all().order_by('first_name')
     return render(request, 'lecturers/get_lecturers.html',
                   {'lecturers': lecturers})
 
 
 def get_groups(request):
-    groups = Group.objects.all().order_by('-course')
+    groups = Group.objects.all().order_by('course')
     return render(request, 'groups/get_groups.html',
                   {'groups': groups})
 
@@ -40,16 +46,66 @@ def add_teacher(request):
             new_teacher = comment_form.save(commit=False)
             new_teacher.save()
 
-    return render(request, 'teachers/add_teacher.html', {'form': form})
+    return render(request, 'lecturers/add_lecture.html', {'form': form})
 
 
 def add_group(request):
     form = AddGroupForm()
     if request.method == 'POST':
-        comment_form = AddGroupForm(data=request.POST)
+        comment_form = AddLecturerForm(data=request.POST)
         if comment_form.is_valid():
-            new_course = comment_form.save(commit=False)
-            new_course.save()
+            new_teacher = comment_form.save(commit=False)
+            new_teacher.save()
 
-    return render(request, 'groups/add_group.html', {'form': form})
+    return render(request, 'lecturers/add_lecture.html', {'form': form})
 
+
+def edit_lecturer(request, id):
+    lecturer = get_object_or_404(Lecturer, id=id)
+    if request.method == 'POST':
+        form = AddLecturerForm(request.POST, instance=lecturer)
+        if form.is_valid():
+            lecturer.save()
+            return redirect('get_lecturers')
+
+    form = AddLecturerForm(instance=lecturer)
+    return render(request, 'lecturers/edit_lecture.html', {'form': form})
+
+
+def delete_lecturer(request, id):
+    Lecturer.objects.filter(id=id).delete()
+    return redirect('get_lecturers')
+
+
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        form = AddStudentForm(request.POST, instance=student)
+        if form.is_valid():
+            student.save()
+            return redirect('get_students')
+
+    form = AddStudentForm(instance=student)
+    return render(request, 'students/edit_student.html', {'form': form})
+
+
+def delete_student(request, id):
+    Student.objects.filter(id=id).delete()
+    return redirect('get_students')
+
+
+def edit_group(request, id):
+    group = get_object_or_404(Group, id=id)
+    if request.method == 'POST':
+        form = AddGroupForm(request.POST, instance=group)
+        if form.is_valid():
+            group.save()
+            return redirect('get_groups')
+
+    form = AddGroupForm(instance=group)
+    return render(request, 'groups/edit_group.html', {'form': form})
+
+
+def delete_group(request, id):
+    Group.objects.filter(id=id).delete()
+    return redirect('get_groups')
